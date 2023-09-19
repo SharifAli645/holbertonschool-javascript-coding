bac62142ff3ca769e2a258c1f2b6617cbfa84d93
SharifAli645 <120497885+SharifAli645@users.noreply.github.com>
2023-09-19 15:34:13 -0500

    Update 1-writeme.js

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
-const file = process.argv[1];
-const content = process.argv[2];
+const file = process.argv[2];
+const content = process.argv[3];
 const fs = require('fs');
 
 fs.writeFile(file, content, (error) => {
