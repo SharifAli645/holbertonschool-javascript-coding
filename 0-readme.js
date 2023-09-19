5170ffdeb08573f66f4ca4380f1b71dbf03a7c33
SharifAli645 <120497885+SharifAli645@users.noreply.github.com>
2023-09-19 10:30:45 -0500

    Delete 0-readme.js

--- 0-readme.js
+++
@@ -1,9 +0,0 @@
-const file = process.argv[2];
-const fs = require('fs');
-fs.readFile(file, 'utf-8', (error, data) => {
-  if (error) {
-    console.error(error);
-    return;
-  }
-  console.log(data);
-});
