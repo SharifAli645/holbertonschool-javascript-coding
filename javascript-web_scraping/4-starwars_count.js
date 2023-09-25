ab562e92d51c61637091aedf08b5ce61fa27833d
SharifAli645 <pandemiaonirica@gmail.com>
2023-09-25 04:43:17 +0000

    js script

---
+++ javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+const request = require('request');
+const api = process.argv[2];
+
+request(api, (error, response, body) => {
+  if (error) {
+    console.error(error);
+  }
+  const resp = JSON.parse(body);
+  let cnt = 0;
+  let idx = 0;
+  while (resp.results[idx]) {
+    resp.results[idx].characters.forEach((value) => {
+      if (value.endsWith('/18/')) {
+        cnt++;
+      }
+    });
+    idx++;
+  }
+  console.log(cnt);
+}); 
