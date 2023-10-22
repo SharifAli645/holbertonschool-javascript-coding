332a69fab07a6322a79b2ba5ad8e92690f577e86
SharifAli645 <pandemiaonirica@gmail.com>
2023-10-22 13:56:06 -0500

    js function

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -3,12 +3,12 @@ const countStudents = require('./3-read_file_async');
 
 const hostname = '127.0.0.1';
 const port = 1245;
-
+/* eslint-disable */
 const app = http.createServer((req, res) => {
   const url = req.url;
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
 
   if (url === '/') {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.end('Hello Holberton School!');
   } else if (url === '/students') {
     res.write('This is the list of our students\n');
