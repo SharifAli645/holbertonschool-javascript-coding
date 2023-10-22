1896e30bba8b7a178fb39f96c3ca8ef2d6c4e1e8
SharifAli645 <pandemiaonirica@gmail.com>
2023-10-22 10:09:12 -0500

    js script

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -3,7 +3,7 @@ console.log('Welcome to Holberton School, what is your name?');
 
 process.stdin.on('readable', () => {
   const name = process.stdin.read();
-  process.stdout.write(`Your name is: ${name}`);
+  if (name) { process.stdout.write(`Your name is: ${name}`); }
 });
 
 process.on('exit', () => {
