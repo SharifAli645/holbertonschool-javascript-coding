b8d799c3d62dd783d5ccca9217226f2277e2dde0
SharifAli645 <pandemiaonirica@gmail.com>
2023-10-22 12:42:50 -0500

    js function

--- Node_JS_basic/4-http.js
+++ Node_JS_basic/4-http.js
@@ -3,11 +3,13 @@ const http = require('http');
 const hostname = '127.0.0.1';
 const port = 1245;
 
-const server = http.createServer((req, res) => {
+const app = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
   res.end('Hello Holberton School!');
 });
 
-server.listen(port, hostname, () => {
+app.listen(port, hostname, () => {
   console.log(`Servidor escuchando en http://127.0.0.1:${port}/`);
 });
+
+module.exports = app;
